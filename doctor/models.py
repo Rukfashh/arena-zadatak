d3e21260301d80fc165339d62bb1baea7e0851cf
Rukfashh <farucini@gmail.com>
2022-12-04 14:15:28 +0100

    fixed an error not leting onto site but login still not working

--- doctor/models.py
+++ doctor/models.py
@@ -1,11 +1,20 @@
-from doctor import db
+from doctor import db, login_manager
 from doctor import bcrypt
+from flask_login import UserMixin
 
-class Doctor(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return Doctor.query.get(int(user_id))
+
+class Doctor(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
+
+    @property
+    def password(self):
+        return self.password
 
     @password.setter
     def password(self, plain_text_password):
--- doctor/routes.py
+++ doctor/routes.py
@@ -2,21 +2,22 @@ from doctor import app
 from flask import render_template , flash, url_for, redirect
 from doctor.models import Patient, Doctor, PatientTomorrow
 from doctor.forms import LoginForm
-from flask_login import login_user
+from flask_login import login_user 
+from doctor import db
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    form= LoginForm()  
-    if form.validate_on_submit():
-        attempted_user = Doctor.query.get(form.username.data).first()
+     form= LoginForm()  
+     if form.validate_on_submit():
+        attempted_user = Doctor.query.filter_by(username=form.username.data).first()
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
             flash('Success! You are logged in as: {attempted_user.username}', category='success')
             return redirect(url_for('dashboard_page'))  
         else:
-            flash('Username and password are not matching! Please try again', category='danger')
-    return render_template('login.html', form=form)
+            flash('Username and password are not matching! Please try again', category='danger') 
+     return render_template('login.html', form=form)
 
 @app.route('/dashboard')
 def dashboard_page():
